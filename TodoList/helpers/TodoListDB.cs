4f33d2ed7a6df49f3737fb8c25eb186b4eae1c4a
abel031 <abel031@users.noreply.github.com>
2021-01-07 13:42:47 +0100

    Agregar archivos de proyecto.

---
+++ TodoList/helpers/TodoListDB.cs
@@ -0,0 +1,23 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TodoList.model;
+
+namespace TodoList.helpers
+{
+    public class TodoListDB
+    {
+        static readonly Lazy<SQLiteAsyncConnection> lazyInitializer = new Lazy<SQLiteAsyncConnection>(() =>
+        {
+            return new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
+        });
+
+        public static SQLiteAsyncConnection ConectionDatabase => lazyInitializer.Value;
+
+        public TodoListDB()
+        {
+            ConectionDatabase.CreateTableAsync<Tarea>();
+        }
+    }
+}
---
+++ TodoList/model/Tarea.cs
@@ -0,0 +1,78 @@
+﻿using TodoList.helpers;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoList.model
+{
+    [Table("Tareas")]
+    public class Tarea : NotifyPropertyBase
+    {
+        [PrimaryKey,AutoIncrement]
+        private long ID { get; set; }
+        private string _FechaCreacion;
+        private string _FechaFinalizacion;
+        private string _FechaPlazo;
+        private string _Descripcion;
+        private bool _Edicion;
+
+        public Tarea()
+        {
+            FechaCreacion = DateTime.Now.ToString("dd/MM/yyyy");
+            FechaFinalizacion = DateTime.Now.ToString("dd/MM/yyyy");
+            FechaPlazo = DateTime.Now.ToString("dd/MM/yyyy");
+            Edicion = false;
+        }
+
+        public string FechaCreacion
+        {
+            get { return _FechaCreacion; }
+            set
+            {
+                _FechaCreacion = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string FechaFinalizacion
+        {
+            get { return _FechaFinalizacion; }
+            set
+            {
+                _FechaFinalizacion = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string FechaPlazo
+        {
+            get { return _FechaPlazo; }
+            set
+            {
+                _FechaPlazo = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Descripcion
+        {
+            get { return _Descripcion; }
+            set
+            {
+                _Descripcion = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool Edicion
+        {
+            get { return _Edicion; }
+            set
+            {
+                _Edicion = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
